483203ee285dd4f8d34ca6f2913eccc3de4e5598
morgadodesarrollador <morgadodesarrollador@gmail.com>
2025-11-10 14:18:02 +0100

    estrucutra DevOps

--- Docker/Caronte/dockerfiles/base/ubuntu/admin/start.sh
+++ Docker/Caronte/dockerfiles/base/ubuntu/admin/start.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-tail -f /dev/null
+newUser(){
+    useradd -rm -d /home/morgado -s /bin/bash morgado
+    echo "morgado:1234" | chpasswd
+    echo "Bienvenido Morgado ... " > /home/morgado/bienvenida.txt
+}
+
+main(){
+    newUser
+    tail -f /dev/null
+}
+
+main
